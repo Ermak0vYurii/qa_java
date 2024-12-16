710f6c68e2cff13cb2b6bcd8722669ab9ed9c55e
Ermak0vYurii <grantovod1@gmail.com>
2024-12-16 22:53:34 +0500

    FelineTest getKittens(); getKittens(int);

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,3 +1,21 @@
+import com.example.Feline;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
 public class FelineTest {
+
+    @Test
+    public void getKittensWithArgumentReturnArgumentTest() {
+        Feline feline = new Feline();
+        assertEquals(5, feline.getKittens(5));
+    }
+
+    @Test
+    public void getKittensWithoutArgumentReturnOneTest() {
+        Feline feline = new Feline();
+        assertEquals(1, feline.getKittens());
+    }
 
 }
