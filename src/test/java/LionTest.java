b87acd1e8c95110b415311b59a4c5f8c6ec958de
Ermak0vYurii <grantovod1@gmail.com>
2024-12-17 14:24:37 +0500

    LionTest 76%

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,2 +1,37 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void doesHaveManeMaleTrueTest() throws Exception {
+        Lion lionMale = new Lion("Самец", feline);
+        assertTrue(lionMale.doesHaveMane());
+    }
+    @Test
+    public void doesHaveManeFemaleFalseTest() throws Exception {
+        Lion lionFemale = new Lion("Самка", feline);
+        assertFalse(lionFemale.doesHaveMane());
+    }
+    @Test
+    public void testConstructorWithInvalidSex() throws Exception {
+        String sex = "Львица";
+        assertThrows(Exception.class, () -> new Lion(sex, feline));
+    }
+
+    @Test
+    public void getKittensTest(){
+
+    }
+
 }
